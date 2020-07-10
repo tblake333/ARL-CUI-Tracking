99dbe11246d14d5c98ab16a56aaf58c52a481d12
Tristan Blake <tristanb@utexas.edu>
2020-07-10 13:08:13 -0700

    updated styles for cancel button

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -5,6 +5,9 @@
         .brand {
             background: #4288cf !important;
         }
+        .cancel {
+            background: #c23c32 !important;
+        }
         .brand-text {
             color: #4288cf !important;
         }
@@ -12,6 +15,11 @@
             max-width: 460px;
             margin: 20px auto;
             padding: 20px;
+        }
+        textarea {
+            border: none;
+            border-bottom: 1px solid #9e9e9e;
+            transition: box-shadow .3s, border .3s, -webkit-box-shadow .3s;
         }
     </style>
 </head>
